54ba82ca2b383f1c27ac05bd775d36dadecac836
Doomse <Hoefer@fzi.de>
2026-02-25 17:11:09 +0100

    Add types for builtins and references

--- ros_bt_py/ros_bt_py/data_types.py
+++ ros_bt_py/ros_bt_py/data_types.py
@@ -27,22 +27,28 @@
 # POSSIBILITY OF SUCH DAMAGE.
 import abc
 from copy import deepcopy
+from importlib import import_module
+from inspect import getmodule
 import json
+from types import NoneType
 from typing import Any, Generic, Optional, Self, Type, TypeGuard, TypeVar
-from typeguard import TypeCheckError, check_type, typechecked
+from typeguard import typechecked
 
 from ros_bt_py.vendor.result import Result, Ok, Err
 
 from ros_bt_py_interfaces.msg import NodeDataType
 
 
-class DataContainer(abc.ABC):
+ANY = TypeVar("ANY")
+
+
+class DataContainer(abc.ABC, Generic[ANY]):
 
     type_identifier: int
     allow_dynamic: bool
     allow_static: bool
     is_static: bool
-    _value: Any
+    _value: ANY
     _updated: bool
 
     def __init__(
@@ -50,15 +56,20 @@ class DataContainer(abc.ABC):
         allow_dynamic: bool = True,
         allow_static: bool = False,
         is_static: bool | None = None,
-        value: Any = None,
+        value: Optional[ANY] = None,
     ) -> None:
         """
         This method calls `set_value` with the given value,
-            so subclasses need to insure that method is functional
+            so subclasses need to ensure that method is functional
             before calling `super().__init__`.
         This also raises `ValueError` if the given value can't be assigned
         """
         super().__init__()
+
+        if not hasattr(self, "type_identifier"):
+            raise RuntimeError(
+                "All concrete implementations have to specify a type_identifier"
+            )
 
         if not allow_dynamic and not allow_static:
             raise RuntimeError(
@@ -100,15 +111,21 @@ class DataContainer(abc.ABC):
         }
 
     @classmethod
-    def from_msg(cls, msg: NodeDataType) -> Result["DataContainer", str]:
+    def from_msg(cls, msg: NodeDataType) -> Result[Self, str]:
         if not hasattr(cls, "type_identifier"):
             raise NotImplementedError("Called on abstract base class")
         if msg.type_identifier != cls.type_identifier:
             return Err("Wrong type identifier")
-        return Ok(cls(**cls._dict_from_msg(msg)))
+        obj = cls(**cls._dict_from_msg(msg))
+        match obj.deserialize_value(msg.serialized_value):
+            case Err(e):
+                return Err(e)
+            case Ok(None):
+                pass
+        return Ok(obj)
 
     @abc.abstractmethod
-    def set_value(self, value: Any) -> Result[None, str]:
+    def set_value(self, value: ANY) -> Result[None, str]:
         """
         Subclasses should validate and clean incoming values
             before calling `super().set_value` to assign them.
@@ -119,7 +136,7 @@ class DataContainer(abc.ABC):
         self._updated = True
         return Ok(None)
 
-    def get_value(self) -> Result[Any, None]:
+    def get_value(self) -> Result[ANY, None]:
         """
         Subclasses should wrap this to include proper type constraints.
         """
@@ -127,10 +144,10 @@ class DataContainer(abc.ABC):
             return Err(None)
         return Ok(self._value)
 
-    def is_updated(self):
+    def is_updated(self) -> bool:
         return self._updated
 
-    def reset_updated(self):
+    def reset_updated(self) -> None:
         self._updated = False
 
     @abc.abstractmethod
@@ -161,10 +178,18 @@ class DataContainer(abc.ABC):
             allow_dynamic=self.allow_dynamic,
             allow_static=self.allow_static,
             is_static=self.is_static,
+            serialized_value=self.serialize_value(),
         )
 
-    @abc.abstractmethod
     def serialize_value(self) -> str:
+        match self.get_value():
+            case Err(None):
+                return ""
+            case Ok(v):
+                return self._serialize_value(v)
+
+    @abc.abstractmethod
+    def _serialize_value(self, value: ANY) -> str:
         raise NotImplementedError("Can't serialize a base class value")
 
     @abc.abstractmethod
@@ -172,11 +197,18 @@ class DataContainer(abc.ABC):
         raise NotImplementedError("Can't deserialize a base class value")
 
 
+class TypeContainerMixin(abc.ABC):
+
+    @abc.abstractmethod
+    def get_value_field(self) -> Result[Type[DataContainer], None]:
+        raise NotImplementedError("Can't get value field for base class")
+
+
 BUILTIN = TypeVar("BUILTIN", bool, int, float, str, list, dict, bytes)
 
 
 @typechecked
-class BuiltinContainer(DataContainer, Generic[BUILTIN]):
+class BuiltinContainer(DataContainer[BUILTIN]):
 
     _type: Type[BUILTIN]
     _value: Optional[BUILTIN]
@@ -208,12 +240,8 @@ class BuiltinContainer(DataContainer, Generic[BUILTIN]):
             return False
         return super().is_compatible(other)
 
-    def serialize_value(self) -> str:
-        match self.get_value():
-            case Err(None):
-                return ""
-            case Ok(v):
-                return json.dumps(v)
+    def _serialize_value(self, value: BUILTIN) -> str:
+        return json.dumps(obj=value)
 
     def deserialize_value(self, ser_value: str) -> Result[None, str]:
         value = json.loads(ser_value)
@@ -221,7 +249,7 @@ class BuiltinContainer(DataContainer, Generic[BUILTIN]):
 
 
 @typechecked
-class BoolContainer(BuiltinContainer[bool]):
+class BoolType(BuiltinContainer[bool]):
 
     type_identifier = NodeDataType.BOOL_TYPE
     _type = bool
@@ -278,7 +306,7 @@ class NumericContainer(BuiltinContainer[NUM]):
 
 
 @typechecked
-class IntContainer(NumericContainer[int]):
+class IntType(NumericContainer[int]):
 
     type_identifier = NodeDataType.INT_TYPE
     _type = int
@@ -301,7 +329,7 @@ class IntContainer(NumericContainer[int]):
 
 
 @typechecked
-class FloatContainer(NumericContainer[float]):
+class FloatType(NumericContainer[float]):
 
     type_identifier = NodeDataType.FLOAT_TYPE
     _type = float
@@ -394,25 +422,25 @@ class IterableContainer(BuiltinContainer[ITER]):
         return type_msg
 
 
-class StringContainer(IterableContainer[str]):
+class StringType(IterableContainer[str]):
     type_identifier = NodeDataType.STRING_TYPE
     _type = str
     _value = ""
 
 
-class ListContainer(IterableContainer[list]):
+class ListType(IterableContainer[list]):
     type_identifier = NodeDataType.LIST_TYPE
     _type = list
     _value = []
 
 
-class DictContainer(IterableContainer[dict]):
+class DictType(IterableContainer[dict]):
     type_identifier = NodeDataType.DICT_TYPE
     _type = dict
     _value = {}
 
 
-class BytesContainer(IterableContainer[bytes]):
+class BytesType(IterableContainer[bytes]):
     type_identifier = NodeDataType.BYTES_TYPE
     _type = bytes
     _value = b"\x00"
@@ -422,7 +450,7 @@ class BytesContainer(IterableContainer[bytes]):
     max_length = 1
     strict_length = True
 
-    def serialize_value(self) -> str:
+    def _serialize_value(self, value: bytes) -> str:
         match self.get_value():
             case Err(None):
                 return ""
@@ -435,3 +463,210 @@ class BytesContainer(IterableContainer[bytes]):
         except ValueError:
             return Err(f"Given string {ser_value} isn't valid hexcode")
         return self.set_value(value)
+
+
+BUILTIN_TYPE_MAP = {
+    bool: BoolType,
+    int: IntType,
+    float: FloatType,
+    str: StringType,
+    list: ListType,
+    dict: DictType,
+    bytes: BytesType,
+}
+
+
+def serialize_class(cls: type) -> str:
+    type_name = cls.__name__
+    module = getmodule(cls)
+    module_name = module.__name__ + "." if module is not None else ""
+    return module_name + type_name
+
+
+def deserialize_class(ser_cls: str) -> Result[type, str]:
+    *mod_path, cls_name = ser_cls.split(".")
+    try:
+        module = import_module(".".join(mod_path))
+    except ValueError:
+        return Err(f"Type {ser_cls} has an invalid module path")
+    cls = getattr(module, cls_name, None)
+    if cls is None:
+        return Err(f"Type {ser_cls} can't be found")
+    return Ok(cls)
+
+
+class BuiltinType(TypeContainerMixin, DataContainer[type]):
+
+    type_identifier = NodeDataType.BUILTIN_TYPE
+    _value = int
+
+    def __init__(self, valid_types=BUILTIN_TYPE_MAP.keys(), *args, **kwargs) -> None:
+        self.valid_types = valid_types
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def _dict_from_msg(cls, msg: NodeDataType) -> dict:
+        config = super()._dict_from_msg(msg)
+        valid_types: list[type] = []
+        for option in msg.value_options:
+            match deserialize_class(option):
+                case Err(_):
+                    continue
+                case Ok(c):
+                    valid_types.append(c)
+        config["valid_types"] = valid_types
+        return config
+
+    def set_value(self, value: type) -> Result[None, str]:
+        if value not in self.valid_types:
+            return Err(
+                f"Type {value} is a valid type. Valid types: {list(self.valid_types)}"
+            )
+        return super().set_value(value)
+
+    def is_compatible(self, other: DataContainer) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        for type_elem in other.valid_types:
+            if type_elem not in self.valid_types:
+                return False
+        return super().is_compatible(other)
+
+    def serialize_type(self) -> NodeDataType:
+        type_msg = super().serialize_type()
+        type_msg.value_options = []
+        for type_elem in self.valid_types:
+            type_msg.value_options.append(serialize_class(type_elem))
+        return type_msg
+
+    def _serialize_value(self, value: type) -> str:
+        return serialize_class(value)
+
+    def deserialize_value(self, ser_value: str) -> Result[None, str]:
+        match deserialize_class(ser_value):
+            case Err(e):
+                return Err(e)
+            case Ok(value):
+                return self.set_value(value)
+
+    def get_value_field(self) -> Result[Type[DataContainer], None]:
+        match self.get_value():
+            case Err(None):
+                return Err(None)
+            case Ok(v):
+                value = v
+        if value not in BUILTIN_TYPE_MAP.keys():
+            return Err(None)
+        return Ok(BUILTIN_TYPE_MAP[value])
+
+
+class ReferenceType(DataContainer[Any]):
+
+    type_identifier = NodeDataType.REFERENCE_TYPE
+    _value: NoneType = None
+    _reference: str
+    _container_map: dict[str, DataContainer[Any]] = {}
+    _inner_type: Optional[DataContainer[Any]] = None
+
+    def __init__(
+        self,
+        reference: str,
+        allow_dynamic: bool = True,
+        allow_static: bool = False,
+        is_static: bool | None = None,
+    ) -> None:
+        super().__init__(allow_dynamic, allow_static, is_static, None)
+        self._reference = reference
+
+    def _set_inner_type(self) -> Result[None, str]:
+        if self._reference not in self._container_map:
+            return Err(f"Given reference {self._reference} is invalid")
+        ref_obj = self._container_map[self._reference]
+        if not isinstance(ref_obj, TypeContainerMixin):
+            return Err("Reference doesn't implement TypeMixin")
+        match ref_obj.get_value_field():
+            case Err(None):
+                return Err("Target didn't yield a value field")
+            case Ok(c):
+                inner_cls = c
+        self._inner_type = inner_cls(
+            allow_dynamic=self.allow_dynamic,
+            allow_static=self.allow_static,
+            is_static=self.is_static,
+        )
+        return Ok(None)
+
+    def set_type_map(self, new_map: dict[str, DataContainer[Any]]):
+        self._container_map = new_map
+        # Ignore errors on setting inner type, it does not HAVE to be set at this point
+        self._set_inner_type()
+
+    def inner_from_msg(self, msg: NodeDataType) -> Result[None, str]:
+        match self._set_inner_type():
+            case Err(e):
+                return Err(e)
+            case Ok(None):
+                pass
+        assert (
+            self._inner_type is not None
+        ), "We just assigned an inner type without issues"
+        inner_cls = self._inner_type.__class__
+        msg_copy = deepcopy(msg)
+        msg_copy.type_identifier = inner_cls.type_identifier
+        match inner_cls.from_msg(msg):
+            case Err(e):
+                return Err(e)
+            case Ok(t):
+                self._inner_type = t
+                return Ok(None)
+
+    @classmethod
+    def _dict_from_msg(cls, msg: NodeDataType) -> dict:
+        config = super()._dict_from_msg(msg)
+        config.pop("value")
+        config["reference"] = msg.reference_target
+        return config
+
+    def set_value(self, value: Any) -> Result[None, str]:
+        if self._inner_type is None:
+            return Err("Can't set value on reference without target")
+        return self._inner_type.set_value(value)
+
+    def get_value(self) -> Result[Any, None]:
+        if self._inner_type is None:
+            return Err(None)
+        return self._inner_type.get_value()
+
+    def is_updated(self) -> bool:
+        if self._inner_type is None:
+            return False
+        return self._inner_type.is_updated()
+
+    def reset_updated(self) -> None:
+        if self._inner_type is None:
+            return None
+        return self._inner_type.reset_updated()
+
+    def is_compatible(self, other: DataContainer) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self._reference == other._reference
+
+    def serialize_type(self) -> NodeDataType:
+        if self._inner_type is None:
+            type_msg = super().serialize_type()
+        else:
+            type_msg = self._inner_type.serialize_type()
+        type_msg.reference_target = self._reference
+        return type_msg
+
+    def _serialize_value(self, value: None) -> str:
+        if self._inner_type is None:
+            return ""
+        return self._inner_type.serialize_value()
+
+    def deserialize_value(self, ser_value: str) -> Result[None, str]:
+        if self._inner_type is None:
+            # Accept and ignore all serialized values if no inner type is set.
+            return Ok(None)
+        return self._inner_type.deserialize_value(ser_value)
